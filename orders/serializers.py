a907a0cab5c81bba77b9e0d815bd5e3f8e957d39
foython <foython@gmail.com>
2025-04-24 20:40:24 +0600

    final

--- orders/serializers.py
+++ orders/serializers.py
@@ -24,10 +24,14 @@ class OrderSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         order_items_data = validated_data.pop('order_items')
-        order = Order.objects.create(**validated_data)
-        for item_data in order_items_data:
-            OrderItem.objects.create(order=order, **item_data)
-        return order
+        user = self.context['request'].user
+        if user.role == 'CUSTOMER':
+            order = Order.objects.create(**validated_data)
+            for item_data in order_items_data:
+                OrderItem.objects.create(order=order, **item_data)
+                return order
+        
+        raise serializers.ValidationError("Only customer can create oders.")
 
     def update(self, instance, validated_data):
         order_items_data = validated_data.pop('order_items', None)
